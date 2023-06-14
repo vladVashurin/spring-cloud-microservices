8ff108e2ecf5dcc376909eb10cab037d46c3214a
Vlad Vashurin <vlad.vashurin@bk.ru>
2023-06-14 11:56:30 +0300

    add configs

--- account-service/src/main/java/com/account/AccountApplication.java
+++ account-service/src/main/java/com/account/AccountApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories
 public class AccountApplication{
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
